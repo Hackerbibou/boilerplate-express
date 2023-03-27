dbc6f3cbc093b6af1ad03d0e62aa737647a1c6b6
Abiboulayed <79129877+Hackerbibou@users.noreply.github.com>
2023-03-27 17:39:30 -0400

    Node and express set up

--- myApp.js
+++ myApp.js
@@ -1,31 +1,15 @@
 let express = require('express');
+
 let app = express();
+console.log("Hello World");
 
+app.get("/",(req,res)=>{
+    res.sendFile(__dirname+"/views/index.html")
+})
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.use("/",(req,res)=>{
+    res.sendFile(__dirname+"/public/style.css")
+})
 
 
 
